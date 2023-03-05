c2e6a7f334275c4dd624a6981d24718425229687
Shreeyash-Naik <shreeyashnaikofficial@gmail.com>
2023-03-05 14:15:28 +0530

    feat: go exercise

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/shreeyashnaik/proto-go
 go 1.17
 
 require (
+	github.com/go-faker/faker/v4 v4.1.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/text v0.8.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
 
+	"github.com/go-faker/faker/v4"
 	pb "github.com/shreeyashnaik/proto-go/proto"
 	"google.golang.org/protobuf/proto"
 )
@@ -65,15 +69,68 @@ func doFile(p proto.Message) {
 }
 
 func main() {
-	fmt.Println(doSimple())
-	fmt.Println(doComplex())
-	fmt.Println(doEnumeration())
+	// fmt.Println(doSimple())
+	// fmt.Println(doComplex())
+	// fmt.Println(doEnumeration())
 
-	doOneof(&pb.Result_Id{Id: 1})
-	doOneof(&pb.Result_Msg{Msg: "Shreeyash"})
+	// doOneof(&pb.Result_Id{Id: 1})
+	// doOneof(&pb.Result_Msg{Msg: "Shreeyash"})
 
-	fmt.Println(doMaps())
+	// fmt.Println(doMaps())
 
-	doFile(doSimple())
+	// doFile(doSimple())
 
+	doAddressBook()
+
+}
+
+func ReadFile(fpath string, m proto.Message) {
+	b, err := ioutil.ReadFile(fpath)
+	if err != nil {
+		log.Println("Unable to open file")
+		return
+	}
+
+	if err := proto.Unmarshal(b, m); err != nil {
+		log.Println("Unable to unmarshal file")
+		return
+	}
+
+	fmt.Println(m)
+}
+
+func WriteFile(fpath string, m proto.Message) {
+	b, err := proto.Marshal(m)
+	if err != nil {
+		log.Println("Cannot serialize to bytes")
+		return
+	}
+
+	if err := ioutil.WriteFile(fpath, b, 0644); err != nil {
+		log.Println("Unable to write to file")
+		return
+	}
+}
+
+func doAddressBook() {
+	abs := &pb.AddressBook{}
+
+	for i := 0; i < 4; i++ {
+		abs.People = append(abs.People, &pb.Person{
+			Name:  faker.Name(),
+			Id:    rand.Int31(),
+			Email: faker.Email(),
+			Phones: []*pb.Person_PhoneNumber{
+				{
+					Number: faker.Phonenumber(),
+					Type:   1,
+				},
+			},
+		})
+	}
+
+	WriteFile("addressbook.bin", abs)
+
+	absNew := &pb.AddressBook{}
+	ReadFile("addressbook.bin", absNew)
 }
